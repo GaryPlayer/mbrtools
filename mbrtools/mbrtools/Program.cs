07afa520c9f60bf7289e700e814f875bff68416e
Gary Player <33352105+GaryPlayer@users.noreply.github.com>
2018-03-31 18:56:54 +0200

    Add files via upload

---
+++ mbrtools/mbrtools/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mbrtools
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          
+            Console.Title = "MBR Tools by Gary Player Ltd.";
+            Console.WriteLine("MBR Tools 1.0 by Gary Player Ltd.");
+            Console.WriteLine(" ");
+            Console.WriteLine("M => FIXMBR  recreates the MBR (Master Boot Sector),it helps with removing roootkits or malware");
+            Console.WriteLine("B => FIXBOOT  recreates the boot directory");
+            while (Console.ReadKey().Key != ConsoleKey.M) {
+                Console.WriteLine("Fixing MBR...");
+                System.Threading.Thread.Sleep(5000);
+                Console.WriteLine("MBR has been fixed!");
+                Console.ReadKey();
+                
+
+            }
+
+
+        }
+    }
+}
